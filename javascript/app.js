d07adc663498e1388a4715defce2723c559fdd45
Allison Amaral <amaralli@uw.edu>
2016-05-21 11:25:10 -0700

    now with files

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//signin functionality to allow response without refresh
+
+angular.module('explanation', [])
+    .controller('SignInController', function($scope /*, $http, $window*/) {
+
+    }); 
