9e9c421811457ffef0e444304c7d9774a1fca046
Allison Amaral <amaralli@uw.edu>
2016-05-24 14:17:04 -0700

    Added Angular, and started fixing JSON

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,26 @@
 'use strict';
 
-//signin functionality to allow response without refresh
+//state choice functionality to allow response without refresh
 
 angular.module('explanation', [])
-    .controller('SignInController', function($scope /*, $http, $window*/) {
+    .controller('StateSelectController', function($scope, $http /*, $window*/) {
+    	//selected state
+    	$scope.currentState = "";
+    	//all states
+    	$scope.states = "";
+
+    	//populates the drop down
+    	$http.get('stateData.json')
+    		.success(function(data) {
+    			$scope.states = data[0];
+    		})
+    		.error(function() {
+    			console.log("Failed to load JSON")
+    		});
+
+    	//changes the state the user has selected every time they select something from the drop down.
+    	$scope.selectState = function(selectedState) {
+    		$scope.currentState = selectedState;
+    	}
 
     }); 
