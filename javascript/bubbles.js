2d7095fdbbbdff563316b5e93435b87958c5f6f2
C Nicholas Mao <mao@tacktechnologies.com>
2016-06-08 01:59:00 -0700

    remade the bubbles! bubbles are now called by stage so we can move them between stages. stage 1 is made.

--- javascript/bubbles.js
+++ javascript/bubbles.js
@@ -9,7 +9,9 @@ window.Bubbles = (function() {
             right: 50
         }
 
-        bubbles.data;
+        bubbles.voterData;
+        bubbles.candidateData;
+
         bubbles.canvasSelector;
 
         bubbles.canvasHeight;
@@ -18,13 +20,20 @@ window.Bubbles = (function() {
         bubbles.chartHeight;
         bubbles.chartWidth;
 
-        bubbles.rowSize = 10;
-        bubbles.bubbleRadius = 10;
+        bubbles.heightMid;
+        bubbles.widthMid;
 
-        bubbles.bubs;
+        bubbles.bubbleRadius = 10;
+        bubbles.bubbleDistance = 40;
+        bubbles.horizontalDistance = 100;
+        bubbles.voterStartingPoint;
+        bubbles.candidateStartingPoint;
+
+        bubbles.voters;
+        bubbles.candidates;
 
         bubbles.build = function() {
-            if (this.canvasSelector == undefined || this.canvasWidth == undefined || this.canvasHeight == undefined || this.data == undefined) {
+            if (this.canvasSelector == undefined || this.canvasWidth == undefined || this.canvasHeight == undefined || this.voterData == undefined) {
                 console.log("Not all required fields have been set yet.");
                 return this;
             }
@@ -39,26 +48,45 @@ window.Bubbles = (function() {
             var chartG = canvasSvg.append('g')
                 .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
-            this.bubs = chartG.selectAll('circle').data(this.data);
+            this.voters = chartG.selectAll('#voters').data(this.voterData);
 
-            this.bubs.enter().append('circle')
-                .attr('r', 10)
-                .style('fill', 'none')
-                .attr('cx', function(d, i) { return ((10 + 15) * (i % 15)) })
-                .attr('cy', function(d, i) { return ((10 + 15) * ((Math.floor(i / 15)) - 1)) })
+            this.voters.enter().append('circle')
+                .attr('r', this.bubbleRadius)
+                .attr('class', "voters")
+                .style('fill', 'white')
+                .attr('cx', function(d, i) { return bubbles.widthMid })
+                .attr('cy', function(d, i) { return bubbles.voterStartingPoint + i * bubbles.bubbleDistance })
 
-            this.bubs.transition()
-                .duration(100)
-                .delay(function(d, i) { return i * 50 })
-                .style('fill', function(d) { return bubbles.determineColor(d) } )
-                .attr('cx', function(d, i) { return ((10 + 15) * (i % 15)) })
-                .attr('cy', function(d, i) { return ((10 + 15) * (Math.floor(i / 15))) })
+            this.candidates = chartG.selectAll('#candidates').data(this.candidateData);
+
+            this.candidates.enter().append('circle')
+                .attr('r', this.bubbleRadius * 1.5)
+                .attr('class', "candidates")
+                .style('fill', 'white')
+                .attr('cx', function(d, i) { return bubbles.widthMid })
+                .attr('cy', function(d, i) { return bubbles.candidateStartingPoint + i * bubbles.bubbleDistance * 2})
 
             return this;
         }
 
+        bubbles.stage1 = function() {
+            this.voters.transition()
+                .duration(1000)
+                .delay(function(d, i) { return i * 50 })
+                .style('fill', function(d) { return bubbles.determineColor(d) })
+                .attr('cx', function(d, i) { return bubbles.widthMid - bubbles.horizontalDistance })
+                .attr('cy', function(d, i) { return bubbles.voterStartingPoint + i * bubbles.bubbleDistance })
+
+            this.candidates.transition()
+                .duration(1000)
+                .delay(function(d, i) { return i * 50 })
+                .style('fill', 'black')
+                .attr('cx', function(d, i) { return bubbles.widthMid + bubbles.horizontalDistance })
+                .attr('cy', function(d, i) { return bubbles.candidateStartingPoint + i * bubbles.bubbleDistance * 2})
+        }
+
         bubbles.sort = function() {
-            this.bubs.transition()
+            this.voters.transition()
                 .duration(1000)
                 .attr('cy', function(d, i ) { return ((10 + 15) * (Math.floor(i / 15)) + 70 * bubbles.determineBucket(d))  })
         }
@@ -78,6 +106,12 @@ window.Bubbles = (function() {
         bubbles.finalCalculations = function() {
             this.chartWidth = this.canvasWidth - this.margin.left - this.margin.right;
             this.chartHeight = this.canvasHeight - this.margin.top - this.margin.bottom;
+
+            this.heightMid = this.chartHeight / 2;
+            this.widthMid = this.chartWidth / 2;
+
+            this.voterStartingPoint = this.heightMid - (((this.voterData.length / 2) - 0.5) * this.bubbleDistance)
+            this.candidateStartingPoint = this.heightMid - (((this.candidateData.length / 2) - 0.5) * this.bubbleDistance * 2)
 
             return this;
         }
@@ -109,14 +143,14 @@ window.Bubbles = (function() {
             return this;
         }
 
-        bubbles.setData = function(data) {
-            this.data = data;
+        bubbles.setVoterData = function(voterData) {
+            this.voterData = voterData;
 
             return this;
         }
 
-        bubbles.setRowSize = function(rowSize) {
-            this.rowSize = rowSize;
+        bubbles.setCandidateData = function(candidateData) {
+            this.candidateData = candidateData;
 
             return this;
         }
